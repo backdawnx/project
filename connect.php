df84e03a1ecaea9a9c03e1046614c1cc21c11969
backdawnx <you@example.c>
2024-01-29 10:34:16 +0700

    first commit

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "0932128365zazas";
+$dbname = "project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo"Lung Tis</br>";
+echo"<h4>By Tananchai CR</h4>";
+?>
+<h2>Welcome to Website<?php echo date("y-m-d")?></h2>  
+<?php
+$year = 2024;
+echo "year"."$year";
+
+?>
+</body>
+</html>
